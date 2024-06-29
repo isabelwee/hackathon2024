328cb59bd7d05d0783d5d987979a368a2a92792e
Amber Wong <amber.wong32@gmail.com>
2024-06-29 17:58:12 +1000

    add bonfires to homepage

--- bonfire/src/pages/HomePage.js
+++ bonfire/src/pages/HomePage.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Header from '../components/Header';
+import AddBonfire from "../components/AddBonfire";
+import fire from "../images/fire.jpg"
 
 function HomePage() {
     return (
-        <Header />
-        // <div>hOME eherhe</div>
+        <>
+        <Header/>
+        <div
+        style={{
+            minHeight: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column-reverse'
+        }}
+        >
+            {/* <Header/> */}
+            <div className="bonfire">
+
+                <img src={fire}
+                style={{
+                    maxWidth: '400px',
+                    margin: '50px',
+                }}
+                ></img>
+
+            </div>
+            <div style={{ position: 'relative'}}>aeojihbo</div>
+        </div>
+        
+    </>
     );
 }
 
