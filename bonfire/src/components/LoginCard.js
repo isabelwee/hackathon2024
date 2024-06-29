d5284acf733ca52aa6646803f9e4e48088e2e673
Pip <pratishtha.thiharie@gmail.com>
2024-06-29 18:46:18 +1000

    card in progess

--- bonfire/src/components/LoginCard.js
+++ bonfire/src/components/LoginCard.js
@@ -1,24 +1,75 @@
 import React from 'react';
 import "../fonts/fonts.css";
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Typography from "@mui/material/Typography";
+import TextField from '@mui/material/TextField';
 
 function LoginButton() {
-  const buttonStyle = {
-    textTransform: 'lowercase',
+  const cardStyle = {
     backgroundColor: '#FFF1E1',
+    width: '20em',
+    height: '18em',
+    textAlign: 'center',
+}
+
+const headerStyle = {
+    textAlign: 'center',
+    fontFamily: 'More Sugar',
     color: '#7A5895',
-    fontFamily: 'More Sugar'
-    
-  }
+    fontSize: '22px'
+}
+
+const fieldStyle = {
+    textAlign: 'left',
+    fontFamily: 'More Sugar',
+    color: '#7A5895',
+    fontSize: '16px'
+}
+
+const helpStyle = {
+    textAlign: 'left',
+    fontFamily: 'Gaegu',
+    color: '#7A5895',
+    fontSize: '16px'
+}
+
+
   return (
-    <card>
+
+    <Card style={cardStyle}>
+        <CardContent>
+            <Typography style={headerStyle}>
+                Log into your account
+            </Typography>
+            <div style={fieldStyle}>
+                email:
+                <div>
+                    <TextField
+                    required
+                    id="outlined-required"
+                    />
+                </div>
+            </div>
+            <div style={fieldStyle}>
+                password:
+                <div>
+                    <TextField
+                    id="outlined-password-input"
+                    type="password"
+                    autoComplete="current-password"
+                    />
+                </div>
+                <Typography style={helpStyle} sx={{textDecoration: 'underline'}}>
+                    forgot your password?
+                </Typography>
+            </div>
+
+        </CardContent>
+
+    </Card>
           
-    <Button variant="contained" color="primary" style={buttonStyle} disableElevation>
-      Log into your account
-    </Button>
-    </card>
+
   );
 }
 
