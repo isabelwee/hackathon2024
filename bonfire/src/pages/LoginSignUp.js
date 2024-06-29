35b3e3202b29754d9004726cc437b0f2b37330fa
Isabel Wee <i.wee@student.unsw.edu.au>
2024-06-29 17:08:45 +1000

    pulled

--- bonfire/src/pages/LoginSignUp.js
+++ bonfire/src/pages/LoginSignUp.js
@@ -1,16 +1,19 @@
 import React from "react";
 import '../App.css';
+import "../fonts/fonts.css";
+import LoginButton from "../components/LoginButton";
 
 function LoginSignUp() {
   const containerStyle = {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     height: '100vh',
   };
 
   const titleStyle = {
-    fontFamily: 'MoreSugar, Arial, sans-serif',
+    fontFamily: 'More Sugar',
     fontSize: '70px',
     fontWeight: 'bold',
     color: '#F1B150',
@@ -18,7 +21,10 @@ function LoginSignUp() {
 
   return (
       < div style={containerStyle}>
-        <div className="bonfire-title" style={titleStyle}>bonfire</div>
+        <div className="bonfire-title" style={titleStyle}>
+          bonfire
+        </div>
+        <LoginButton />
       </div>
 
   );
