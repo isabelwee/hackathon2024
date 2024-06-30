0bd2580753890da4e28ffd92c3d9c83d313fa8aa
Isabel Wee <i.wee@student.unsw.edu.au>
2024-06-30 11:24:39 +1000

    pulled changes

--- bonfire/src/pages/Dummy.js
+++ bonfire/src/pages/Dummy.js
@@ -4,13 +4,32 @@ import "../fonts/fonts.css";
 import BucketListModal from "../components/BucketListModal";
 import CreateBonfireModal from "../components/CreateBonfireModal";
 import UploadImagesModal from "../components/UploadImagesModal"
+import { Stack, Box, Checkbox, Button, IconButton } from '@mui/material';
+
+import Typography from '@mui/material/Typography';
 
 function LoginSignUp() {
+  const [bucketListModalOpen, setBucketListModalOpen] = React.useState(false);
+
+  const handleBucketListModalOpen = () => setBucketListModalOpen(true);
+  const handleBucketListModalClose = () => setBucketListModalOpen(false);
+
+  const typographyStyle = {
+    textAlign: 'left',
+    fontFamily: 'Gaegu',
+    color: '#394B6E',
+  };
+
 
   return (
       <div>
-        <BucketListModal />
+        <Typography variant="h4" sx={{...typographyStyle, cursor: 'pointer', '&:hover': {fontWeight: 'bold'}}} onClick={handleBucketListModalOpen}>+ Add new bucket list item!</Typography>
+        <BucketListModal 
+        open={bucketListModalOpen} 
+        handleClose={handleBucketListModalClose} 
+      />
         <CreateBonfireModal />
+        <Typography variant="h4" sx={{...typographyStyle, cursor: 'pointer', '&:hover': {fontWeight: 'bold'}}} onClick={handleBucketListModalOpen}>Upload images</Typography>
         <UploadImagesModal />
       </div>
 
